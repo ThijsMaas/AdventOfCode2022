bc4689d9fe9fa8a26bfaae357e8b336b253cb009
Thijs <tpwmaas@gmail.com>
2022-12-06 21:46:21 +0100

    Setup to boilerplate for the rest of the solutions

---
+++ src/solutions/day14.rs
@@ -0,0 +1,16 @@
+// use itertools::Itertools;
+// use std::{
+//     fs::File,
+//     io::{BufRead, BufReader},
+//     path::Path,
+// };
+
+pub fn solution() {
+    // let base = Path::new(env!("CARGO_MANIFEST_DIR")).join("data/example.txt");
+    // let mut reader = BufReader::new(File::open(base).unwrap());
+    // let mut data = String::new();
+    // reader.read_line(&mut data).expect("");
+    // let lines = reader.lines();
+    // for line in lines {}
+    todo!("Implement solution!")
+}
